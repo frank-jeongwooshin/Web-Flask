4ce38d0ae1780603b1d533c232a792c3b0018f2f
frank-jeongwooshin <maecsan12@gmail.com>
2022-04-07 00:11:34 +0900

    2022-04-07

---
+++ JS.js
@@ -0,0 +1,25 @@
+// JS 문법
+// 변수 
+let number = 3;
+let name = "Frank";
+let realNum = 3.14;
+const NUM = 3;
+const NAME = "FRANK";
+const PIE = 3.141592;
+
+// alert vs consol
+alert(NAME);
+console.log(realNum);
+
+// input
+let client = prompt("What is Your Name ?");
+alert(`Hello, ${client}`);
+
+// function
+function add(num1, num2)
+{
+    return num1 + num2;
+}
+
+let result = add(3, 6);
+console.log(result);
